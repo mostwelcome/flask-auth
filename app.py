f2f2ad28c5f201a8144c969e41d68cd99bf68d16
mostwelcome <tuadutta13@gmail.com>
2021-09-14 10:11:50 +0530

    added few changes for post request

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
+# app.config['SECRET_KEY'] = 'any-secret-key-you-choose'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-##CREATE TABLE IN DB
+#
+# @app.before_first_request
+# def create_table():
+#     db.create_all()
+#
+#
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-#Line below only required once, when creating DB. 
-# db.create_all()
 
 
 @app.route('/')
@@ -25,9 +27,28 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=["POST", "GET", "DELETE"])
 def register():
+    print('here')
+    if request.method == "POST":
+        new_user = User(
+            email=request.form.get('email'),
+            name=request.form.get('name'),
+            password=request.form.get('password')
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        return redirect(url_for("secrets"))
+
     return render_template("register.html")
+
+
+@app.route('/swag')
+def swag():
+    print('here')
+    return {'a': 1}
 
 
 @app.route('/login')
